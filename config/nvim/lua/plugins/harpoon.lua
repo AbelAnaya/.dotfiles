b2e7b691360af699f223443f528c0545e70c0a45
AbelAnaya <abelanaya97@gmail.com>
2023-10-12 00:08:35 +0200

    chore(nvim): Fix harpoon bind

--- config/nvim/lua/plugins/harpoon.lua
+++ config/nvim/lua/plugins/harpoon.lua
@@ -9,7 +9,7 @@ return {
     vim.keymap.set("n", "<leader>ha", mark.add_file, { desc = "Mark file with harpoon" })
     vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Toggle harpoon quick menu" })
 
-    vim.keymap.set("n", "<leader>hn>", function() ui.nav_next() end, { desc = "Go to next harpoon mark" })
-    vim.keymap.set("n", "<leader>hp>", function() ui.nav_prev() end, { desc = "Go to previous harpoon mark" })
+    vim.keymap.set("n", "<leader>hn", function() ui.nav_next() end, { desc = "Go to next harpoon mark" })
+    vim.keymap.set("n", "<leader>hp", function() ui.nav_prev() end, { desc = "Go to previous harpoon mark" })
   end,
 }
