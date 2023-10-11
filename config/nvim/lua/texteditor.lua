c96acad0ab0fd1a8ba051969b37e10b31b0ef9c3
AbelAnaya <abelanaya97@gmail.com>
2023-10-12 00:07:54 +0200

    chore(nvim): Set RNU

--- config/nvim/lua/texteditor.lua
+++ config/nvim/lua/texteditor.lua
@@ -1,8 +1,11 @@
--- Set EOL, Whitespace, Trailing, Whitespace
+-- Set Ruler, TextWidth and indentation configuration
 vim.opt.textwidth = 79
 vim.opt.colorcolumn = {120}
 vim.opt.breakindent = true
 vim.opt.linebreak = true
+
+-- Set relative number
+vim.opt.rnu = true
 
 -- Set EOL, Whitespace, Trailing, Whitespace
 vim.opt.listchars:append{eol = "¬", space = "·", tab = "->", precedes = "<", extends = ">"}
