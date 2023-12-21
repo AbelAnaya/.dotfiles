70820092c78f6929dd7642b96affe09d64ae307c
AbelAnaya <abelanaya97@gmail.com>
2023-12-21 23:08:25 +0100

    feat(nvim): Improve nvim-tree-sitter configuration

--- config/nvim/lua/plugins/nvim-treesitter.lua
+++ config/nvim/lua/plugins/nvim-treesitter.lua
@@ -1,6 +1,7 @@
 return {
     -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    event = { "BufReadPre", "BufNewFile" },
     dependencies = {
       'nvim-treesitter/nvim-treesitter-textobjects',
     },
@@ -11,10 +12,32 @@ return {
 
         treesitter.setup {
             -- Add languages to be installed here that you want installed for treesitter
-            ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim' },
+            ensure_installed = {
+                'c',
+                'cpp',
+                'go',
+                'lua',
+                'python',
+                'rust',
+                'tsx',
+                'javascript',
+                'typescript',
+                'vimdoc',
+                'vim',
+                'markdown',
+                'markdown_inline',
+                'dockerfile',
+                'gitignore',
+                'bash',
+                'query',
+                'yaml',
+                'xml',
+                'html',
+                'json',
+            },
 
             -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-            auto_install = false,
+            auto_install = true,
 
             highlight = { enable = true },
             indent = { enable = true },
@@ -27,50 +50,10 @@ return {
                 node_decremental = '<M-space>',
               },
             },
-            textobjects = {
-              select = {
-                enable = true,
-                lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                keymaps = {
-                  -- You can use the capture groups defined in textobjects.scm
-                  ['aa'] = '@parameter.outer',
-                  ['ia'] = '@parameter.inner',
-                  ['af'] = '@function.outer',
-                  ['if'] = '@function.inner',
-                  ['ac'] = '@class.outer',
-                  ['ic'] = '@class.inner',
-                },
-              },
-              move = {
-                enable = true,
-                set_jumps = true, -- whether to set jumps in the jumplist
-                goto_next_start = {
-                  [']m'] = '@function.outer',
-                  [']]'] = '@class.outer',
-                },
-                goto_next_end = {
-                  [']M'] = '@function.outer',
-                  [']['] = '@class.outer',
-                },
-                goto_previous_start = {
-                  ['[m'] = '@function.outer',
-                  ['[['] = '@class.outer',
-                },
-                goto_previous_end = {
-                  ['[M'] = '@function.outer',
-                  ['[]'] = '@class.outer',
-                },
-              },
-              swap = {
-                enable = true,
-                swap_next = {
-                  ['<leader>a'] = '@parameter.inner',
-                },
-                swap_previous = {
-                  ['<leader>A'] = '@parameter.inner',
-                },
-              },
-            },
         }
+
+        -- Enable nvim-ts-context-commentstring plugin for better comments
+        require('ts_context_commentstring').setup {}
+
     end,
 }
