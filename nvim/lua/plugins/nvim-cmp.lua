93466663c4677b7a9121bf852ef348831af8a4b1
AbelAnaya <abelanaya97@gmail.com>
2024-05-08 00:33:01 +0200

    refactor(nvim): change scroll mappins in cmp

--- nvim/lua/plugins/nvim-cmp.lua
+++ nvim/lua/plugins/nvim-cmp.lua
@@ -43,8 +43,8 @@ return {
           mapping = cmp.mapping.preset.insert {
             ['<C-n>'] = cmp.mapping.select_next_item(cmp_select),
             ['<C-p>'] = cmp.mapping.select_prev_item(cmp_select),
-            ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-            ['<C-f>'] = cmp.mapping.scroll_docs(4),
+            ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-d>'] = cmp.mapping.scroll_docs(4),
             ['<C-Space>'] = cmp.mapping.complete {},
             ['<C-e>'] = cmp.mapping.abort(),
             ['<C-y>'] = cmp.mapping.confirm {
