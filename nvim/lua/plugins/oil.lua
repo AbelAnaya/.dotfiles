f95664924b9da18c8eac50151c2796084b0e57ac
abelanaya <abelanaya97@gmail.com>
2024-05-16 13:20:49 +0200

    feat(nvim): Replace nvim-tree by oil

---
+++ nvim/lua/plugins/oil.lua
@@ -0,0 +1,16 @@
+return {
+	"stevearc/oil.nvim",
+	opts = {},
+	-- Optional dependencies
+	dependencies = { "nvim-tree/nvim-web-devicons" },
+	config = function()
+		require("oil").setup({
+			delete_to_trash = true,
+			view_options = {
+				-- Show files and directories that start with "."
+				show_hidden = true,
+			},
+		})
+		vim.keymap.set("n", "-", "<cmd>Oil<CR>", { desc = "Open parent directory" })
+	end,
+}
