2f652230cde470f5c99639a64442d48c9d1803e4
abelanaya <abelanaya97@gmail.com>
2024-05-16 12:22:44 +0200

    feat(nvim): Add several hints to lsp

--- nvim/lua/plugins/lsp/lsp.lua
+++ nvim/lua/plugins/lsp/lsp.lua
@@ -105,6 +105,44 @@ return {
 
           nmap('<leader>fb', ':Format<CR>', '[F]ormat [B]uffer')
 
+            -- The following two autocommands are used to highlight references of the
+            -- word under your cursor when your cursor rests there for a little while.
+            --    See `:help CursorHold` for information about when this is executed
+            --
+            -- When you move your cursor, the highlights will be cleared (the second autocommand).
+            if client and client.server_capabilities.documentHighlightProvider then
+                local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
+                vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+                    buffer = bufnr,
+                    group = highlight_augroup,
+                    callback = vim.lsp.buf.document_highlight,
+                })
+
+                vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+                    buffer = bufnr,
+                    group = highlight_augroup,
+                    callback = vim.lsp.buf.clear_references,
+                })
+
+                vim.api.nvim_create_autocmd('LspDetach', {
+                    group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+                    callback = function(event2)
+                        vim.lsp.buf.clear_references()
+                        vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event2.buf }
+                    end,
+                })
+            end
+
+            -- The following autocommand is used to enable inlay hints in your
+            -- code, if the language server you are using supports them
+            --
+            -- This may be unwanted, since they displace some of your code
+            if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
+                nmap('<leader>th', function()
+                    vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+                end, '[T]oggle Inlay [H]ints')
+            end
+
         end
 
         -- nvim-cmp supports additional completion capabilities, so broadcast that to servers
