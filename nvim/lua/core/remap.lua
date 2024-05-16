dd4aaf3d9e5370bddc1d6c6a8c90e9deb4efa523
abelanaya <abelanaya97@gmail.com>
2024-05-16 22:48:29 +0200

    feat(nvim): Add window resize keymaps

--- nvim/lua/core/remap.lua
+++ nvim/lua/core/remap.lua
@@ -3,19 +3,19 @@
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
 
 -- Using nerd font
 vim.g.have_nerd_font = true
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set({ "n", "v" }, "<Space>", "<Nop>", { silent = true })
 
 -- Remap for dealing with word wrap (in case enabled, this setup is `set nowrap`)
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+vim.keymap.set("n", "k", "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set("n", "j", "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Move selected code up and down
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
@@ -35,8 +35,8 @@ vim.keymap.set("n", "<C-s>", "<cmd>w<CR>")
 vim.keymap.set("n", "Q", "<nop>")
 
 -- Greatest remaps ever
-vim.keymap.set("x", "<leader>p", [["_dP]], { desc = 'Paste without modifying registers' })
-vim.keymap.set({ 'n', 'v' }, "<leader>d", [["_d]], { desc = 'Delete without modifying registers' })
+vim.keymap.set("x", "<leader>p", [["_dP]], { desc = "Paste without modifying registers" })
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]], { desc = "Delete without modifying registers" })
 
 -- sessionizer integration in nvim remap
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer.sh<CR>")
@@ -64,28 +64,34 @@ vim.cmd("noremap <silent> <s-Down> :<C-U>TmuxNavigateDown<cr>")
 vim.cmd("noremap <silent> <s-Up> :<C-U>TmuxNavigateUp<cr>")
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list as loclist' })
-vim.keymap.set('n', '<leader>Q', vim.diagnostic.setqflist, { desc = 'Open diagnostics list as qflist' })
+vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic message" })
+vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic message" })
+vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Open floating diagnostic message" })
+vim.keymap.set("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Open diagnostics list as loclist" })
+vim.keymap.set("n", "<leader>Q", vim.diagnostic.setqflist, { desc = "Open diagnostics list as qflist" })
 
 vim.diagnostic.config({
-    virtual_text = true,
-    float = {
-        focusable = false,
-        style = 'minimal',
-        border = 'rounded',
-        source = 'always',
-        header = "",
-        prefix = "",
-    },
+	virtual_text = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
 })
 
+-- Resize window keymap
+vim.keymap.set("n", "<M-h>", "<c-w>5<", { desc = "Resize window up" })
+vim.keymap.set("n", "<M-l>", "<c-w>5>", { desc = "Resize window up" })
+vim.keymap.set("n", "<M-k>", "<c-w>5+", { desc = "Resize window up" })
+vim.keymap.set("n", "<M-j>", "<c-w>5-", { desc = "Resize window up" })
+
 -- Center screen after jump keymaps
-vim.keymap.set('n', '<C-o>', '<C-o>zz')
-vim.keymap.set('n', '<C-i>', '<C-i>zz')
-vim.keymap.set('n', '<C-d>', '<C-d>zz')
-vim.keymap.set('n', '<C-u>', '<C-u>zz')
-vim.keymap.set('n', 'n', 'nzzzv')
-vim.keymap.set('n', 'N', 'Nzzzv')
+vim.keymap.set("n", "<C-o>", "<C-o>zz")
+vim.keymap.set("n", "<C-i>", "<C-i>zz")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
