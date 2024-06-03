3c91b184bdd738a7860a5cdf468b240f89400bdd
abelanaya <abelanaya97@gmail.com>
2024-06-03 22:36:23 +0200

    chore(build): Move custom lua files to a specific custom folder to avoid missunderstanding with plugins convention

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -23,7 +23,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
+require("lazy").setup({ { import = "custom" } }, {
 	change_detection = {
 		notify = false,
 	},
