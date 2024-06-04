c8bfac6e9cb234fc90f98898af47d81760df18ca
abelanaya <abelanaya97@gmail.com>
2024-06-04 22:53:16 +0200

    chore(nvim): Change mini surround by nvim surround

--- nvim/.config/nvim/lua/custom/mini.lua
+++ nvim/.config/nvim/lua/custom/mini.lua
@@ -9,13 +9,6 @@ return {
         --  - ci'  - [C]hange [I]nside [']quote
         require("mini.ai").setup({ n_lines = 500 })
 
-        -- Add/delete/replace surroundings (brackets, quotes, etc.)
-        --
-        -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
-        -- - sd'   - [S]urround [D]elete [']quotes
-        -- - sr)'  - [S]urround [R]eplace [)] [']
-        require("mini.surround").setup()
-
         -- Highlight trailing whitespaces and newlines. Adds functionality to trim both:
         -- - MiniTrailspace.trim()
         -- - MiniTrailspace.trim_last_lines()
