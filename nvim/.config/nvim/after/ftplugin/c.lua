ee07b58c6e2b2a1c1275b11cea3e6bab4a15c148
AbelAnaya <abelanaya97@gmail.com>
2024-06-27 19:51:13 +0200

    feat(nvim): Add switch header/source for C/C++ files

--- nvim/.config/nvim/after/ftplugin/c.lua
+++ nvim/.config/nvim/after/ftplugin/c.lua
@@ -1,1 +1,4 @@
 vim.opt_local.formatoptions:remove("o")
+
+-- Switch header and source file using LSP
+vim.keymap.set("n", "<leader>o", "<cmd>ClangdSwitchSourceHeader<cr>", { desc = "Switch Source/Header (C/C++)" })
