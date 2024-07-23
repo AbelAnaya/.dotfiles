5bf42836152e8d75638eae9444974af232feddf8
AbelAnaya <abelanaya97@gmail.com>
2024-07-23 14:56:06 +0200

    chore(nvim): Remove automatic open of aerial tree

--- nvim/.config/nvim/lua/custom/aerial.lua
+++ nvim/.config/nvim/lua/custom/aerial.lua
@@ -66,12 +66,6 @@ return {
             -- Set to false to disable
             highlight_on_jump = 300,
 
-            open_automatic = function(bufnr)
-                return vim.api.nvim_buf_line_count(bufnr) > 80
-                    and aerial.num_symbols(bufnr) > 4
-                    and not aerial.was_closed()
-            end,
-
             -- Run this command after jumping to a symbol (false will disable)
             post_jump_cmd = "normal! zz",
 
