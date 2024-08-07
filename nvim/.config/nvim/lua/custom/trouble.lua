27f5b24f3efa4d8571d5ec6f7df30976ff817bef
abelanaya <abelanaya97@gmail.com>
2024-08-08 00:40:57 +0200

    chore(nvim): Add keymaps for trouble

--- nvim/.config/nvim/lua/custom/trouble.lua
+++ nvim/.config/nvim/lua/custom/trouble.lua
@@ -9,14 +9,54 @@ return {
             use_diagnostic_signs = true,
         })
 
-        vim.keymap.set("n", "<leader>tt", "<cmd>TroubleToggle<CR>", { silent = true, desc = "[T]oggle [T]rouble" })
+        vim.keymap.set(
+            "n",
+            "<leader>td",
+            "<cmd>Trouble diagnostics toggle<CR>",
+            { silent = true, desc = "[T]oggle [D]iagnostics [T]oggle" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>tD",
+            "<cmd>Trouble diagnostics toggle filter.buf=0<CR>",
+            { silent = true, desc = "[T]oggle [D]iagnostics Buffer [T]oggle" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>tq",
+            "<cmd>Trouble loclist toggle<CR>",
+            { silent = true, desc = "[T]oggle [L]oclist [T]oggle" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>tQ",
+            "<cmd>Trouble qflist toggle<CR>",
+            { silent = true, desc = "[T]oggle [Q]flist [T]oggle" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>ts",
+            "<cmd>Trouble symbols toggle<CR>",
+            { silent = true, desc = "[T]oggle [S]ymbols [T]oggle" }
+        )
+
+        vim.keymap.set(
+            "n",
+            "<leader>tl",
+            "<cmd>Trouble lsp toggle focus=false win.position=right<CR>",
+            { silent = true, desc = "[T]oggle [L]sp [T]oggle" }
+        )
 
         vim.keymap.set("n", "<leader>tn", function()
             require("trouble").next({ skip_groups = true, jump = true })
         end, { silent = true, desc = "[T]rouble [N]ext" })
 
         vim.keymap.set("n", "<leader>tp", function()
-            require("trouble").previous({ skip_groups = true, jump = true })
+            require("trouble").prev({ skip_groups = true, jump = true })
         end, { silent = true, desc = "[T]rouble [P]revious" })
     end,
 }
