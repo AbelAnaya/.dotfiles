aadd2364b2bb055ca321d6b8283bee23e94a3080
abelanaya <abelanaya97@gmail.com>
2024-08-08 13:23:40 +0200

    feat(nvim): Show relative path of files in lualine

--- nvim/.config/nvim/lua/custom/lualine.lua
+++ nvim/.config/nvim/lua/custom/lualine.lua
@@ -1,6 +1,7 @@
 return {
     -- Set lualine as statusline
     "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
     -- See `:help lualine.txt`
     opts = {
         options = {
@@ -8,6 +9,9 @@ return {
             theme = "vscode",
             component_separators = { left = "", right = "" },
             section_separators = { left = "", right = "" },
+        },
+        sections = {
+            lualine_c = { { "filename", path = 1 } },
         },
     },
 }
