f76b2fac37a83b0a99e158c02e194c33255d7c15
AbelAnaya <abelanaya97@gmail.com>
2024-08-22 00:55:35 +0200

    refactor(nvim): Rely on LSP Eslint for linting and formatter
    ESlint LSP already handles formatting and linter based in rules of the
    project so it is considered safer to use directly LSP

--- nvim/.config/nvim/lua/custom/nvim-lint.lua
+++ nvim/.config/nvim/lua/custom/nvim-lint.lua
@@ -9,24 +9,25 @@ return {
 
         lint.linters_by_ft = {
             cpp = { "cpplint" },
-            javascript = { "standardjs" },
-            typescript = { "standardjs" },
-            javascriptreact = { "standardjs" },
-            typescriptreact = { "standardjs" },
+            -- For JS and TS use LSP linting
+            -- javascript = { "standardjs" },
+            -- typescript = { "standardjs" },
+            -- javascriptreact = { "standardjs" },
+            -- typescriptreact = { "standardjs" },
             python = { "pylint", "pydocstyle", "mypy" },
             markdown = { "markdownlint" },
         }
 
-        local binary_name = "standard"
-
-        local cmd_string = function()
-            local local_binary = vim.fn.fnamemodify("./node_modules/.bin/" .. binary_name, ":p")
-            return vim.loop.fs_stat(local_binary) and local_binary or binary_name
-        end
-
-        -- Use always local standard
-        local standardjs = lint.linters.standardjs
-        standardjs.cmd = cmd_string()
+        -- local binary_name = "standard"
+        --
+        -- local cmd_string = function()
+        --     local local_binary = vim.fn.fnamemodify("./node_modules/.bin/" .. binary_name, ":p")
+        --     return vim.loop.fs_stat(local_binary) and local_binary or binary_name
+        -- end
+        --
+        -- -- Use always local standard
+        -- local standardjs = lint.linters.standardjs
+        -- standardjs.cmd = cmd_string()
 
         local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
