3e5dbad50839cfec9a1b932b4f4a4b277b0bb7ad
AbelAnaya <abelanaya97@gmail.com>
2024-09-19 23:57:41 +0200

    feat(nvim): Add supermaven as inline copilot

--- nvim/.config/nvim/lua/custom/copilot-chat.lua
+++ nvim/.config/nvim/lua/custom/copilot-chat.lua
@@ -2,7 +2,7 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "canary",
     dependencies = {
-        { "github/copilot.vim" }, -- or github/copilot.vim
+        { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
         { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
     config = function()
--- nvim/.config/nvim/lua/custom/copilot.lua
+++ nvim/.config/nvim/lua/custom/copilot.lua
@@ -1,3 +1,8 @@
 return {
-    "github/copilot.vim",
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    config = function()
+        require("copilot").setup({})
+    end,
 }
