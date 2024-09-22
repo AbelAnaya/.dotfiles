5bc6f613c028767de1c227f93fa44138da53c48a
AbelAnaya <abelanaya97@gmail.com>
2024-09-22 22:09:58 +0200

    feat(nvim): Disable noice commands and messages

--- nvim/.config/nvim/lua/custom/noice.lua
+++ nvim/.config/nvim/lua/custom/noice.lua
@@ -22,83 +22,19 @@ return {
                     ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
                 },
             },
-            views = {
-                cmdline_popup = {
-                    position = {
-                        row = 20,
-                        col = "50%",
-                    },
-                    size = {
-                        width = 60,
-                        height = "auto",
-                    },
-                },
-                popupmenu = {
-                    relative = "editor",
-                    position = {
-                        row = 23,
-                        col = "50%",
-                    },
-                    size = {
-                        width = 60,
-                        height = 10,
-                    },
-                    border = {
-                        style = "rounded",
-                        padding = { 0, 1 },
-                    },
-                    win_options = {
-                        winhighlight = { Normal = "Normal", FloatBorder = "DiagnosticInfo" },
-                    },
-                },
+            cmdline = {
+                enabled = false,
+            },
+            messages = {
+                enabled = false,
             },
             -- you can enable a preset for easier configuration
             presets = {
                 bottom_search = false, -- use a classic bottom cmdline for search
                 command_palette = false, -- position the cmdline and popupmenu together
-                long_message_to_split = true, -- long messages will be sent to a split
+                long_message_to_split = false, -- long messages will be sent to a split
                 inc_rename = false, -- enables an input dialog for inc-rename.nvim
                 lsp_doc_border = true, -- add a border to hover docs and signature help
-            },
-            routes = {
-                -- Route written file message to mini (small right down corner notification)
-                {
-                    filter = {
-                        event = "msg_show",
-                        kind = "",
-                        find = "written",
-                    },
-                    view = "mini",
-                },
-                -- Route undo and replace messages to mini
-                {
-                    filter = {
-                        event = "msg_show",
-                        kind = "",
-                        find = "change",
-                    },
-                    view = "mini",
-                },
-                {
-                    filter = {
-                        event = "msg_show",
-                        kind = "",
-                        find = "line",
-                    },
-                    view = "mini",
-                },
-                -- Hide search count virtual text
-                {
-                    filter = {
-                        event = "msg_show",
-                        kind = "search_count",
-                    },
-                    view = "notify",
-                    opts = {
-                        title = "Search Count",
-                        replace = true,
-                    },
-                },
             },
         })
     end,
