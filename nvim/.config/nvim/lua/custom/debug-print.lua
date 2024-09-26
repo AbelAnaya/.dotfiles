ad5f6f23492e402e8b09809edcf55aecae915289
AbelAnaya <abelanaya97@gmail.com>
2024-09-26 21:55:03 +0200

    feat(nvim): Add configuration for debug-print

--- nvim/.config/nvim/lua/custom/debug-print.lua
+++ nvim/.config/nvim/lua/custom/debug-print.lua
@@ -7,4 +7,26 @@ return {
     -- Remove the following line to use development versions,
     -- not just the formal releases
     version = "*",
+    config = function()
+        require("debugprint").setup({
+            keymaps = {
+                normal = {
+                    plain_below = "<leader>rp",
+                    plain_above = "<leader>rP",
+                    variable_below = "<leader>rv",
+                    variable_above = "<leader>rV",
+                    variable_below_alwaysprompt = "<leader>rc",
+                    variable_above_alwaysprompt = "<leader>rC",
+                    textobj_below = "<leader>ro",
+                    textobj_above = "<leader>rO",
+                    toggle_comment_debug_prints = "<leader>rt",
+                    delete_debug_prints = "<leader>rd",
+                },
+                visual = {
+                    variable_below = "<leader>rv",
+                    variable_above = "<leader>rV",
+                },
+            },
+        })
+    end,
 }
