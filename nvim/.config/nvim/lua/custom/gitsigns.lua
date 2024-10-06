05ffd0de134747268efcbf1a8d090dbce0778576
AbelAnaya <abelanaya97@gmail.com>
2024-10-06 21:01:10 +0200

    Add gitsigns keymaps

--- nvim/.config/nvim/lua/custom/gitsigns.lua
+++ nvim/.config/nvim/lua/custom/gitsigns.lua
@@ -24,7 +24,7 @@ return {
             local gitsigns = require("gitsigns")
             vim.keymap.set("n", "<leader>gp", gitsigns.prev_hunk, { buffer = bufnr, desc = "[G]o to [P]revious Hunk" })
             vim.keymap.set("n", "<leader>gn", gitsigns.next_hunk, { buffer = bufnr, desc = "[G]o to [N]ext Hunk" })
-            vim.keymap.set("n", "<leader>ph", gitsigns.preview_hunk, { buffer = bufnr, desc = "[P]review [H]unk" })
+            vim.keymap.set("n", "<leader>hp", gitsigns.preview_hunk, { buffer = bufnr, desc = "[H]unk [P]review" })
             vim.keymap.set("n", "<leader>gd", gitsigns.diffthis, { buffer = bufnr, desc = "[G]it [D]iff this" })
 
             vim.keymap.set("n", "<leader>gD", function()
@@ -32,6 +32,30 @@ return {
             end, { buffer = bufnr, desc = "[G]it [D]iff against previous commit" })
 
             vim.keymap.set("n", "<leader>gb", gitsigns.blame_line, { buffer = bufnr, desc = "[G]it [B]lame line" })
+
+            vim.keymap.set("n", "<leader>hs", gitsigns.stage_hunk, { buffer = bufnr, desc = "[H]unk [s]tage" })
+            vim.keymap.set("n", "<leader>hr", gitsigns.reset_hunk, { buffer = bufnr, desc = "[H]unk [r]eset" })
+            vim.keymap.set("v", "<leader>hs", function()
+                gitsigns.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
+            end, { buffer = bufnr, desc = "[H]unk [s]tage" })
+            vim.keymap.set("v", "<leader>hr", function()
+                gitsigns.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
+            end, { buffer = bufnr, desc = "[H]unk [r]eset" })
+
+            vim.keymap.set("n", "<leader>hS", gitsigns.stage_buffer, { buffer = bufnr, desc = "[H]unk [S]tage" })
+            vim.keymap.set(
+                "n",
+                "<leader>hu",
+                gitsigns.undo_stage_hunk,
+                { buffer = bufnr, desc = "[H]unk [u]undo stage" }
+            )
+            vim.keymap.set("n", "<leader>hR", gitsigns.reset_buffer, { buffer = bufnr, desc = "[H]unk [R]eset" })
+            vim.keymap.set(
+                "n",
+                "<leader>hd",
+                gitsigns.toggle_deleted,
+                { buffer = bufnr, desc = "[H]unk toggle [d]elete" }
+            )
         end,
     },
 }
